2c1c909cd40239edfccd1e958525023f1b52bafb
Songnian <1127093059@qq.com>
2022-04-14 17:59:20 +0800

    init tabs of easy-music

---
+++ src/views/singer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="singer">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'singer',
+
+}
+</script>
+
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
