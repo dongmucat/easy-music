c47b04082e9b836346f746fcebf2cfcb70e97582
Songnian <1127093059@qq.com>
2022-04-15 16:52:48 +0800

    实现了轮播图

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,78 +1,99 @@
 <template>
   <div class="recommend">
-
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import {getRecommend,getAlbum} from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
 export default {
   name: 'recommend',
+  components:{
+    Slider
+  },
+  data() {
+    return {
+      sliders:[],
+      albums:[],
+    }
+  },
+  async created(){
+    //获取数据
+    const res = await getRecommend();
+    //赋值
+    this.sliders = res.sliders;
+    this.albums = res.albums;
+  }
 
 }
 </script>
-
 <style lang="scss" scoped>
-.recommend {
-  position: fixed;
-  width: 100%;
-  top: 88px;
-  bottom: 0;
-  overflow: scroll;
-  .recommend-content {
-    height: 100%;
-    overflow: hidden;
-    .slider-wrapper {
-      position: relative;
-      width: 100%;
-      height: 0;
-      padding-top: 40%;
+  .recommend {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    overflow: scroll;
+    .recommend-content {
+      height: 100%;
       overflow: hidden;
-      .slider-content {
-        position: absolute;
-        left: 0;
-        top: 0;
+      .slider-wrapper {
+        position: relative;
         width: 100%;
-        height: 100%;
-      }
-    }
-    .recommend-list {
-      .list-title {
-        height: 65px;
-        line-height: 65px;
-        text-align: center;
-        font-size: $font-size-medium;
-        color: $color-theme;
-      }
-      .item {
-        display: flex;
-        box-sizing: border-box;
-        align-items: center;
-        padding: 0 20px 20px 20px;
-
-        .icon {
-          flex: 0 0 60px;
-          width: 60px;
-          padding-right: 20px;
+        height: 0;
+        padding-top: 40%;
+        overflow: hidden;
+        .slider-content {
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
         }
-        .text {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          flex: 1;
-          line-height: 20px;
-          overflow: hidden;
+      }
+      .recommend-list {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
           font-size: $font-size-medium;
+          color: $color-theme;
         }
-        .name {
-          margin-bottom: 10px;
-          color: $color-text;
-        }
-        .title {
-          color: $color-text-d;
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
+          }
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: $font-size-medium;
+          }
+          .name {
+            margin-bottom: 10px;
+            color: $color-text;
+          }
+          .title {
+            color: $color-text-d;
+          }
         }
       }
     }
   }
-}
 </style>
+
