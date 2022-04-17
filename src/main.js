bd75024b884b5c5c757222454209368895fe6ddb
Songnian <1127093059@qq.com>
2022-04-17 15:03:10 +0800

    添加了v-loding自定义指令

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 //图片懒加载
 import lazyPlugin from 'vue3-lazy'
-/*import loadingDirective from '@/components/base/loading/directive'
-import noResultDirective from '@/components/base/no-result/directive'
+import loadingDirective from '@/components/base/loading/directive'
+/*import noResultDirective from '@/components/base/no-result/directive'
 import { load, saveAll } from '@/assets/js/array-store'
 import { FAVORITE_KEY, PLAY_KEY } from '@/assets/js/constant'
 import { processSongs } from '@/service/song' */
@@ -31,5 +31,5 @@ if (historySongs.length > 0) {
 
 createApp(App).use(store).use(router).use(lazyPlugin, {
   loading: require('@/assets/images/default.png')
-}).mount('#app')/*.directive('loading', loadingDirective).directive('no-result', noResultDirective).mount('#app')
+}).directive('loading', loadingDirective).mount('#app')/*.directive('no-result', noResultDirective).mount('#app')
  */
