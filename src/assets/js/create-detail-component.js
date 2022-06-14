d46c86e57c4b7e954fe88ed20199d3aa03aed617
Songnian <1127093059@qq.com>
2022-06-14 18:46:20 +0800

    新增了排行榜和排行歌单详情页

--- src/assets/js/create-detail-component.js
+++ src/assets/js/create-detail-component.js
@@ -5,7 +5,9 @@ import { processSongs } from '@/service/song'
 export default function createDetailComponent(name, key, fetch) {
   return {
     name,
-    components: { MusicList },
+    components: {
+      MusicList
+    },
     props: {
       data: Object
     },
@@ -17,17 +19,20 @@ export default function createDetailComponent(name, key, fetch) {
     },
     computed: {
       computedData() {
-        let ret = null
+        let res = null
         const data = this.data
         if (data) {
-          ret = data
+          res = data
         } else {
           const cached = storage.session.get(key)
-          if (cached && (cached.mid || cached.id + '') === this.$route.params.id) {
-            ret = cached
+          if (
+            cached &&
+            (cached.mid || cached.id + '') === this.$route.params.id
+          ) {
+            res = cached
           }
         }
-        return ret
+        return res
       },
       pic() {
         const data = this.computedData
@@ -40,15 +45,20 @@ export default function createDetailComponent(name, key, fetch) {
     },
     async created() {
       const data = this.computedData
+      console.log(this.$route)
       if (!data) {
+        // 如果没有data则跳转到 /top-list
         const path = this.$route.matched[0].path
         this.$router.push({
           path
         })
         return
       }
+      // 获取歌曲
       const result = await fetch(data)
+      // 处理歌曲并且保存
       this.songs = await processSongs(result.songs)
+      // 取消loading效果
       this.loading = false
     }
   }
