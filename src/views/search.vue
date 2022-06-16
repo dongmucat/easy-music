f214dd4b34dd27ffff2d4984e31ef22175056c61
Songnian <1127093059@qq.com>
2022-06-16 12:43:04 +0800

    新增了搜索结果页面并支持歌手和歌曲的选择

--- src/views/search.vue
+++ src/views/search.vue
@@ -4,7 +4,10 @@
       <search-input v-model="query"></search-input>
     </div>
 
-    <div class="search-content">
+    <div
+      class="search-content"
+      v-show="!query"
+    >
       <div class="hot-keys">
         <h1 class="title">热门搜索</h1>
         <ul>
@@ -19,21 +22,51 @@
         </ul>
       </div>
     </div>
+    <div
+      class="search-result"
+      v-show="query"
+    >
+      <suggest
+        :query="query"
+        @select-song="selectSong"
+        @select-singer="selectSinger"
+      ></suggest>
+    </div>
+    <router-view v-slot="{ Component }">
+      <transition
+        appear
+        name="slide"
+      >
+        <component
+          :is="Component"
+          :singer="selectedSinger"
+        />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script>
 import SearchInput from '@/components/search/search-input'
+import Suggest from '@/components/search/suggest'
 import { ref, watch } from 'vue'
-import { getHotKeys } from '@/service/search'
+import { getHotKeys, search } from '@/service/search'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import storage from 'good-storage'
+import { SINGER_KEY } from '@/assets/js/constant'
 export default {
   name: 'search',
   components: {
-    SearchInput
+    SearchInput,
+    Suggest
   },
   setup() {
     const query = ref('')
     const hotKeys = ref([])
+    const store = useStore()
+    const router = useRouter()
+    const selectedSinger = ref(null)
 
     getHotKeys().then(result => {
       hotKeys.value = result.hotKeys
@@ -43,11 +76,31 @@ export default {
     function addQuery(key) {
       query.value = key
     }
+
+    function selectSong(song) {
+      store.dispatch('addSong', song)
+    }
+
+    function selectSinger(singer) {
+      selectedSinger.value = singer
+      cacheSinger(singer)
+      router.push({
+        path: `/search/${singer.mid}`
+      })
+    }
+
+    function cacheSinger(singer) {
+      storage.session.set(SINGER_KEY, singer)
+    }
+
     return {
       query,
       hotKeys,
+      selectedSinger,
       // methods
-      addQuery
+      addQuery,
+      selectSong,
+      selectSinger
     }
   }
 }
@@ -55,60 +108,60 @@ export default {
 
 <style lang="scss" scoped>
 .search {
-  position: fixed;
-  width: 100%;
-  top: 88px;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  .search-input-wrapper {
-    margin: 20px;
-  }
-  .search-content {
-    flex: 1;
-    overflow: hidden;
-    .hot-keys {
-      margin: 0 20px 20px 20px;
-      .title {
-        margin-bottom: 20px;
-        font-size: $font-size-medium;
-        color: $color-text-l;
-      }
-      .item {
-        display: inline-block;
-        padding: 5px 10px;
-        margin: 0 20px 10px 0;
-        border-radius: 6px;
-        background: $color-highlight-background;
-        font-size: $font-size-medium;
-        color: $color-text-d;
-      }
-    }
-    .search-history {
-      position: relative;
-      margin: 0 20px;
-      .title {
-        display: flex;
-        align-items: center;
-        height: 40px;
-        font-size: $font-size-medium;
-        color: $color-text-l;
-        .text {
-          flex: 1;
-        }
-        .clear {
-          @include extend-click();
-          .icon-clear {
-            font-size: $font-size-medium;
-            color: $color-text-d;
-          }
-        }
-      }
-    }
-  }
-  .search-result {
-    flex: 1;
-    overflow: hidden;
-  }
+	position: fixed;
+	width: 100%;
+	top: 88px;
+	bottom: 0;
+	display: flex;
+	flex-direction: column;
+	.search-input-wrapper {
+		margin: 20px;
+	}
+	.search-content {
+		flex: 1;
+		overflow: hidden;
+		.hot-keys {
+			margin: 0 20px 20px 20px;
+			.title {
+				margin-bottom: 20px;
+				font-size: $font-size-medium;
+				color: $color-text-l;
+			}
+			.item {
+				display: inline-block;
+				padding: 5px 10px;
+				margin: 0 20px 10px 0;
+				border-radius: 6px;
+				background: $color-highlight-background;
+				font-size: $font-size-medium;
+				color: $color-text-d;
+			}
+		}
+		.search-history {
+			position: relative;
+			margin: 0 20px;
+			.title {
+				display: flex;
+				align-items: center;
+				height: 40px;
+				font-size: $font-size-medium;
+				color: $color-text-l;
+				.text {
+					flex: 1;
+				}
+				.clear {
+					@include extend-click();
+					.icon-clear {
+						font-size: $font-size-medium;
+						color: $color-text-d;
+					}
+				}
+			}
+		}
+	}
+	.search-result {
+		flex: 1;
+		overflow: hidden;
+	}
 }
 </style>
